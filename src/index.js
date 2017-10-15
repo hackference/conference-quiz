cfa5218f1750912492d8da9f38b672bcfdbcbef9
Mike Elsmore <mike@elsmore.me>
2017-10-15 19:02:04 +0100

    The app now saves data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Dotenv from 'dotenv';
 import Hapi from 'hapi';
 import Nexmo from 'nexmo';
+import Knex from 'knex';
 
 // Load any local ENV
 Dotenv.load({ silent: true });
@@ -12,6 +13,36 @@ const nexmo = new Nexmo({
   applicationId: process.env.NEXMO_APP_ID,
   privateKey: process.env.NEXMO_APP_ID
 });
+
+// Configure the DB
+const Database = Knex({
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  searchPath: 'knex,public'
+});
+
+// Make sure the tables exist
+Database.schema
+  .createTableIfNotExists('questions', table => {
+    table.increments();
+    table.text('question');
+    table.string('answer_a');
+    table.string('answer_b');
+    table.string('answer_c');
+    table.string('answer_d');
+    table.timestamps(true, true);
+  })
+  .then();
+Database.schema
+  .createTableIfNotExists('answers', table => {
+    table.increments();
+    table.bigInteger('number');
+    table.integer('question').unsigned();
+    table.foreign('question').references('questions.id');
+    table.string('answer');
+    table.timestamps(true, true);
+  })
+  .then();
 
 // Setup Hapi
 const server = new Hapi.Server();
@@ -43,22 +74,70 @@ example
 1 for question 1 and it's answers
 OR
 1a to answer a for question 1`;
+      nexmo.message.sendSms(
+        request.query.to,
+        request.query.msisdn,
+        response,
+        () => {}
+      );
     } else {
-      const question = found[1];
+      const questionNumber = found[1];
       const answer = found[2];
-      response = `Q: ${question}
-A: ${answer}`;
+      if (answer) {
+        Database.insert({
+          number: request.query.msisdn,
+          question: questionNumber,
+          answer: answer
+        })
+          .into('answers')
+          .then(() => {
+            response = `Thank you for the answer.`;
+          })
+          .then(() => {
+            nexmo.message.sendSms(
+              request.query.to,
+              request.query.msisdn,
+              response,
+              () => {}
+            );
+          });
+      } else {
+        Database.select(
+          'question',
+          'answer_a',
+          'answer_b',
+          'answer_c',
+          'answer_d'
+        )
+          .from('questions')
+          .where({ id: questionNumber })
+          .then(data => {
+            response = `Question:
+${data[0].question}
+
+Answers:
+A) ${data[0].answer_a}
+B) ${data[0].answer_b}
+C) ${data[0].answer_c}
+D) ${data[0].answer_d}`;
+          })
+          .then(() => {
+            nexmo.message.sendSms(
+              request.query.to,
+              request.query.msisdn,
+              response,
+              () => {}
+            );
+          });
+      }
     }
-    nexmo.message.sendSms(
-      request.query.to,
-      request.query.msisdn,
-      response,
-      () => {}
-    );
     reply({});
   }
 });
-
+Database.select('question', 'answer_a', 'answer_b', 'answer_c', 'answer_d')
+  .from('questions')
+  .where({ id: 1 })
+  .then(data => {});
 // Start the server
 server.start(err => {
   if (err) {
